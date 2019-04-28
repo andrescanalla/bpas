ba771871a3f7aab1a0bb341492db536a03850cbd
wedelia <pasquinelli@a-w-a.com.ar>
2019-04-27 21:07:22 -0300

    api

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/calendarios/importar', 'DashboardController@importEvent');
-Route::get('/calendarios/settinglocalidades', 'DashboardController@settingLocalidades');
+
