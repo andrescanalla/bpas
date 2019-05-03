851b41c305a5c191119235fe39fdbd121f280408
wedelia <pasquinelli@a-w-a.com.ar>
2019-05-02 23:56:01 -0300

    fd

--- resources/views/departamentos/index.blade.php
+++ resources/views/departamentos/index.blade.php
@@ -217,6 +217,7 @@
                     data: {departamento:departamento,filtro:filtro}
                   })
                   .done(function(data) {
+                    console.log('data:',data.count, filtro);
                     data.forEach( function(valor, indice, array) {                     
                       var request = {
                         query: valor.nombreLocalidad+', santa fe, argentina',
