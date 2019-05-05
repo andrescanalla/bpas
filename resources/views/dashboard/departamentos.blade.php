51c6fe8fadc9258526ef4eab01477938dbec0651
wedelia <pasquinelli@a-w-a.com.ar>
2019-05-05 20:12:11 -0300

    dash depto link

--- resources/views/dashboard/departamentos.blade.php
+++ resources/views/dashboard/departamentos.blade.php
@@ -14,19 +14,19 @@
           <div class="panel-body" style="font-size:13px">
               <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" style="padding-left:0;padding-right:7px"> 
                 <ul class="list-group" style="margin-bottom:0">
-                  <a href="/stock/stock?talle=&tipo=&cat=&est=&marca=&estado=Comprado">
+                  <a href="/departamentos/3">
                     <li class="list-group-item list-group-item-success" style="height: 28px;padding-top:4px" >
                       <span class="badge">{{$belgrano}}/6</span>
                       Belgrano
                     </li>
                   </a>
-                  <a href="/stock/stock?talle=&tipo=&cat=&est=&marca=&estado=Reservado">
+                  <a href="/departamentos/2">
                     <li class="list-group-item list-group-item-info" style="height: 28px;padding-top:4px;margin-top:3px">
                     <span class="badge">{{$caseros}}/14</span>
                       Caseros
                     </li>
                   </a>
-                  <a href="#">
+                  <a href="/departamentos/8">
                     <li class="list-group-item list-group-item-info" style="height: 28px;padding-top:4px;margin-top:3px">
                       <span class="badge">{{$constitucion}}/19</span>
                       Cosntitucion
@@ -36,18 +36,18 @@
               </div>
               <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" style="padding-left:7px; padding-right:0"> 
                 <ul class="list-group" style="margin-bottom:0">
-                  <a href="/stock/stock?talle=&tipo=2&cat=&est=&marca=&estado=">
+                  <a href="/departamentos/7">
                     <li class="list-group-item list-group-item-warning" style="height: 28px;padding:4px 8px 0 8px">
                       <span class="badge">{{$lopez}}/31</span>
                       General Lopez 
                     </li>
                   </a>
-                  <a href="/stock/stock?talle=&tipo=3&cat=&est=&marca=&estado=">
+                  <a href="/departamentos/4">
                     <li class="list-group-item list-group-item-success" style="height: 28px;padding-top:4px;margin-top:3px">
                       <span class="badge">{{$iriondo}}/12</span>
                       Iriondo
                     </li></a>
-                  <a href="#">
+                  <a href="/departamentos/6">
                     <li class="list-group-item list-group-item-warning" style="height: 28px;padding-top:4px;margin-top:3px" >
                       <span class="badge">{{$rosario}}/24</span>
                       Rosario
@@ -57,7 +57,7 @@
               </div>
               <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" style="padding-left:7px; padding-right:0"> 
                 <ul class="list-group" style="margin-bottom:0">
-                  <a href="/stock/stock?talle=&tipo=2&cat=&est=&marca=&estado=">
+                  <a href="/departamentos/5">
                     <li class="list-group-item list-group-item-info" style="height: 28px;padding-top:4px">
                       <span class="badge">{{$san}}/15</span>
                       San Lorenzo 
